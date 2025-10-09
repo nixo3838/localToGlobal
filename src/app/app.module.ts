c32337a833f0ed58967dd65009a10a953e3ecda5
manralamit99-tech <manralamit99@gmail.com>
2025-10-09 17:51:22 +0530

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -47,9 +47,10 @@ import { HeaderComponent } from './GuestComponents/header/header.component';
     {
       provide: HTTP_INTERCEPTORS,
       useClass: GlobalHeaderInterceptor,
-      multi: true
-    }
+      multi: true,
+    },
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class AppModule { }
+export class AppModule {}
